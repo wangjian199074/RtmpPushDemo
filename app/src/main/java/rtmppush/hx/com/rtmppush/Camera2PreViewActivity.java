42a4a681473a43e3044731c850b6f3f8aacf39b8
wangjian199074 <wangjian199074@163.com>
2019-09-25 18:57:57 +0800

    推流Demo第一次提交Init工程
    Signed-off-by: wangjian199074 <wangjian199074@163.com>

---
+++ app/src/main/java/rtmppush/hx/com/rtmppush/Camera2PreViewActivity.java
@@ -0,0 +1,15 @@
+package rtmppush.hx.com.rtmppush;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Camera2PreViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Camera2Preview);
+    }
+
+
+}
